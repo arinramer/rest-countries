d3d93ef340833ab91bb8cf55dd35d52da691d5fb
arinramer <arinramer@gmail.com>
2020-11-29 19:07:20 -0800

    Added region dropdown menu

--- app/src/App.js
+++ app/src/App.js
@@ -14,10 +14,33 @@ const Box = styled.div`
   flex-basis: 21%;
 `
 
+const Dropdown = styled.div`
+  height: 25px;
+  border: .5px solid gray;
+  border-radius: 5px;
+  padding: 10px;
+  user-select: none;
+  &:hover {
+    background-color: lightgray;
+  }
+`
+const Dropdownbox = styled.div`
+  text-align: center;
+  border: .5px solid gray;
+  border-bottom: 0px;
+  padding: 8px;
+  background-color: white;
+  user-select: none;
+  &:hover {
+    background-color: lightgray;
+  }
+`
+
 function App() {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState()
   const [input, setInput] = useState()
+  const [menu, setMenu] = useState(false)
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all')
@@ -32,6 +55,22 @@ function App() {
     setSearch(filteredCountries)
   }
 
+  const menuToggle = e => {
+    e.preventDefault()
+    setMenu(!menu)
+  }
+
+  const menuButton = e => {
+    e.preventDefault()
+    const region = e.target.innerText
+    const filteredRegion = 
+      countries.filter(country => {
+        return country.region === region
+      })
+    setSearch(filteredRegion)
+    menuToggle(e)
+  }
+
   const filteredCountries = 
     countries.filter(country => {
       return country.name.toLowerCase().includes(input)
@@ -42,6 +81,17 @@ function App() {
       <div style={{marginLeft: '3.5%'}}>
         <h1>Where in the world?</h1>
         <input placeholder="Search" type="text" value={input} onChange={handleInput}/>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'flex-end', marginRight: '5%', position: 'relative', marginTop: '-40px', marginLeft: '25%'}}>
+        <Dropdown onClick={menuToggle}>Region {menu ? '↑': '↓'}</Dropdown>
+        <div style={menu ? {display: 'flex', flexDirection: 'column', position: 'absolute', marginTop: '50px'}: {display: 'none'}}>
+          <Dropdownbox onClick={menuButton}>Asia</Dropdownbox>
+          <Dropdownbox onClick={menuButton}>Europe</Dropdownbox>
+          <Dropdownbox onClick={menuButton}>Africa</Dropdownbox>
+          <Dropdownbox onClick={menuButton}>Oceania</Dropdownbox>
+          <Dropdownbox onClick={menuButton}>Americas</Dropdownbox>
+          <Dropdownbox onClick={menuButton} style={{border: '.5px solid gray'}}>Polar</Dropdownbox>
+        </div>
       </div>
       <Container>
       {console.log(countries)}
